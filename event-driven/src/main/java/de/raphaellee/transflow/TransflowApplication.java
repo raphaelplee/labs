0079afc6a58ce0c889e41c4862c6dbfda77df503
Raphael Lee <raphael.p.lee@gmail.com>
2026-05-17 16:51:32 +0200

    build: add transflow-core Maven module with all dependencies

---
+++ event-driven/src/main/java/de/raphaellee/transflow/TransflowApplication.java
@@ -0,0 +1,11 @@
+package de.raphaellee.transflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransflowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransflowApplication.class, args);
+    }
+}
